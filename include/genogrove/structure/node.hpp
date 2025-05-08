696efc764c582c70fc359dea2e52a7f5e158d5a1
riasc <richard.schaefer@zoho.com>
2025-05-08 14:37:14 -0500

    templified node class

--- include/genogrove/structure/node.hpp
+++ include/genogrove/structure/node.hpp
@@ -1,8 +1,70 @@
-//
-// Created by Richard Albin Schaefer on 5/6/25.
-//
-
+/*
+ * SPDX-License-Indentifier: MIT
+ *
+ * Copyright (c) 2025 Richard A. Schäfer
+ *
+ * This file is part of genogrove and is licensed under the terms of the MIT license.
+ * See the LICENSE file in the root of the repository for more information.
+ */
 #ifndef GENOGROVE_STRUCTURE_NODE_HPP
 #define GENOGROVE_STRUCTURE_NODE_HPP
+
+// standard
+#include <vector>
+
+// genogrove
+#include <genogrove/structure/key.hpp>
+
+namespace genogrove::structure {
+    class node {
+        public:
+            node(int k);
+            ~node();
+
+
+            // getter & setter
+            int get_order();
+            void set_order(int k);
+            std::vector<node*> get_keys();
+            void set_keys(std::vector<node*> keys);
+            std::vector<node*>& get_children();
+            void set_children(std::vector<node*> children);
+            node* get_parent();
+            void set_parent(node* parent);
+            void set_next(node* next);
+            node* get_next();
+            void set_is_leaf(bool is_leaf);
+            bool get_is_leaf();
+
+            template<typename key_type>
+            void insert_key(key<int>& key1);
+
+            template<typename key_type>
+            void insert_key(key<key_type>& key);
+
+            template<typename key_type>
+            key_type calc_parent_key(key<key_type>& key);
+
+            void add_child(node* child, int index);
+            node* get_child(int index);
+
+            /*
+             *
+             */
+            void serialize(std::ostream& os);
+            static node* deserialize(std::istream& is, int order);
+
+            void print_keys(std::ostream& os, std::string sep="\t");
+
+
+        private:
+            int order;
+            std::vector<node*> keys;
+            std::vector<node*> children;
+            node* parent;
+            node* next;
+            bool is_leaf;
+    }
+}
 
 #endif //GENOGROVE_STRUCTURE_NODE_HPP
---
+++ src/node.cpp
@@ -0,0 +1,13 @@
+//
+// Created by Richard Albin Schaefer on 5/8/25.
+//
+
+#include <genogrove/structure/node.hpp>
+
+namespace genogrove::structure {
+    node::node(int order) {
+    }
+}
+
+
+
