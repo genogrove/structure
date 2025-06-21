418a8e614f12e0c6d270654008a625377a4af75d
riasc <richard.schaefer@zoho.com>
2025-06-21 23:49:19 +0200

    changed node to correct template to work with specific key types

--- src/key.cpp
+++ src/key.cpp
@@ -56,7 +56,7 @@ namespace genogrove::structure {
             is.read(&type_name[0], type_name_length);
 
             // deserialize the data object
-            key.data = ggt::any_base::deserialize(is, type_name);
+            key1.data = ggt::type_registry::create(type_name);
         }
 
 
--- src/node.cpp
+++ src/node.cpp
@@ -1,13 +1,16 @@
 #include <genogrove/structure/node.hpp>
 
 namespace genogrove::structure {
-    node::node(int order) : order(order), keys{}, children{}, parent{nullptr}, next{nullptr}, is_leaf{false} {}
+    <typename key_type>
+    node<key_type>::node(int order) : order(order), keys{}, children{}, parent{nullptr}, next{nullptr}, is_leaf{false} {}
 
     // getter & setter
+
+
     template<typename key_type>
-    void node::insert_key(key<key_type>& key1) {
+    void node<key_type>::insert_key(key<key_type>& key1) {
         int i = 0;
-        while(i < this->keys.size() && key.)
+        while(i < this->keys.size() && key1.get_value() > this->keys[i].get_value()) { i++; }
 
 
     }
