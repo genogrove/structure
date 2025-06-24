613d1e49e59c2f3eabe79ffe5c0faf65afb204f2
riasc <richard.schaefer@zoho.com>
2025-06-24 23:35:30 +0200

    added templates

--- src/node.cpp
+++ src/node.cpp
@@ -1,20 +1,8 @@
 #include <genogrove/structure/node.hpp>
 
 namespace genogrove::structure {
-    <typename key_type>
-    node<key_type>::node(int order) : order(order), keys{}, children{}, parent{nullptr}, next{nullptr}, is_leaf{false} {}
-
-    // getter & setter
-
-
     template<typename key_type>
-    void node<key_type>::insert_key(key<key_type>& key1) {
-        int i = 0;
-        while(i < this->keys.size() && key1.get_value() > this->keys[i].get_value()) { i++; }
-
-
-    }
-
+    node<key_type>::node(int order) : order(order), keys{}, children{}, parent{nullptr}, next{nullptr}, is_leaf{false} {}
 }
 
 
